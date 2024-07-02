ea25811783434e6556cdcf92a544329e010a2682
Rudresh Soiroji <rudreshsoiroji459@gmail.com>
2024-07-02 07:00:58 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [wordToGuess, setWordToGuess] = useState("hello")
+
+  return (
+    <>
+    <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App
