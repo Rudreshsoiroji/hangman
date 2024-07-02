f0b514d6ff4c71676b63d14b5f837028b540e2ae
Rudresh Soiroji <rudreshsoiroji459@gmail.com>
2024-07-02 19:29:57 +0530

    add word

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
 function HangmanWord() {
+    const word = "test";
+    const guessLetter = ["t"];
   return (
-    <div style={{display: 'flex', fontFamily:"monospace", fontWeight:"bold", gap: "0.25em" ,fontSize: "6rem"}} >
-       <p>test</p>
+    <div style={{display: 'flex', fontFamily:"monospace", fontWeight:"bold", gap: "0.25em" ,fontSize: "6rem", textTransform:"uppercase"}} >
+       {word.split("").map((letter, index)=>{
+return <span style={{borderBottom: "solid 1rem black"}} key={index}>
+    
+    <span style={{visibility: guessLetter.includes(letter) ? "visible" : "hidden"}}>
+        {letter}
+        </span>
+    
+    </span>
+
+       })}
         </div>
   )
 }
