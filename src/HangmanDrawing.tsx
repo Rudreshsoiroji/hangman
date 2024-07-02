5481b20febc3c2601c1eb788a928141054437498
Rudresh Soiroji <rudreshsoiroji459@gmail.com>
2024-07-02 07:43:03 +0530

    add the skeliton drawing

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function HangmanDrawing() {
   return (
-    <div>
+      <div style={{position: 'relative'}}>
+        <div style={{height:"50px", width:"10px", backgroundColor:"black", position:"absolute", top:"0", right:"0"}}></div>
+
+        <div style={{height:"10px", width:"200px", 
+            marginLeft:"120px", backgroundColor:"black"}}></div>
+        
+        <div style={{height:"400px", width:"10px", backgroundColor:"black", marginLeft:"120px"}}></div>
+        <div style={{height:"10px", width:"250px", backgroundColor:"black"}}></div>
       
     </div>
   )
